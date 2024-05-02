e2aa38ae1b9a31ec7e6a4e5dc2ccb7d9bbcd7c97
harrison <harrison@gmail.com>
2024-05-02 12:49:59 +0000

    mathematical function

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,7 +1,13 @@
-export default function divideFunction(numerator, denominator) {
-  if (denominator === 0) {
-    throw new Error('cannot divide by 0');
+export default function guardrail(mathFunction) {
+  const queue = [];
+
+  try {
+    queue.push(mathFunction());
+  } catch (err) {
+    queue.push(String(err));
+  } finally {
+    queue.push('Guardrail was processed');
   }
 
-  return numerator / denominator;
+  return queue;
 }
